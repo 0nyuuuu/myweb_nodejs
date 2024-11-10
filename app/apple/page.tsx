86b0b2be518fca6832cbde9a8da5b9d51a785bb6
0nyuuuu <aqwwetrt@gmail.com>
2024-11-10 15:57:16 +0900

    anjfk

---
+++ app/apple/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { HeartIcon, SendIcon } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className = "flex justify-center items-center w-full h-[100dvh] bg-gray-300">
+      <div className="max-w-sm w-full h-[100dvh] bg-white">
+        <div className="flex justify-between px-4">
+      <span className="font-bold border-4 border-blue-500">lnstagram</span>
+      <div className="flex items-center gap-4 border-2">
+        <button onClick={() => like()}>
+          <HeartIcon className="w-6 h-6"/>
+        </button>
+        <button>
+          <SendIcon className="w-6 h-6"/>
+        </button>
+      </div>
+    </div>
+      </div>
+    </div> 
+  );
+}
+
+function like() {
+  //.log("like!!!");
+  prompt("Like!!")
+  confirm("yes or no!!");
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-
-
 export default function Home() {
   return (
     <div>
-      Commit test
-    </div>
+      <MyButton name="Admin" nickname="user" age={18} today={new Date()}/>
+      </div>
   );
+}
+
+// function MyButton({name} : {name : string}) {
+//   return <button>I am {name}</button>
+// }
+
+type Tparams = {
+  name: string
+  nickname: string
+  age: number
+  today: Date
+}
+
+function MyButton(params: Tparams) {
+  return <button>I am {params.name}. My nickname is {params.nickname}. My age is {params.age}. {params.today.getDate()}</button>
 }
